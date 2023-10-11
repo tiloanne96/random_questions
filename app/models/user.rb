e0a4c6aed6ad5d8503a9e1cddaed73828e4bbfef
tilo <tiloanne96@gmail.com>
2023-10-10 19:38:13 -0700

    Updating code, adding some specs basics tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   validates :email, presence: true
+  validates :name, presence: true
   validates :email, uniqueness: true
   validate :validate_email
 
   def validate_email
+    return false unless email.present?
+    
     email_validation = Truemail.validate(email).result
 
     if email_validation.success
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "User validations" do
+    it "User without name should be invalid" do
+      user = User.new(email: "someemail@mailinator.com")
+
+      expect(user.valid?).to be(false)
+    end
+
+    it "User without email should be invalid" do
+      user = User.new(name: "somename")
+
+      expect(user.valid?).to be(false)
+    end
+
+    it "User with bad email should be invalid" do
+      user = User.new(name: "somename", email: "invalid_email@m@ilinator.com")
+
+      expect(user.valid?).to be(false)
+    end
+
+    it "User with all data should be valid" do
+      user = User.new(name: "somename", email: "valid_email@mailinator.com")
+      
+      expect(user.valid?).to be(true)
+    end
+  end
 end
